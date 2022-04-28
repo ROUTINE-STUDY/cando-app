6784016ac39ba126f92e44f48f34ed11769b27b9
hwang434 <inqbator@knou.ac.kr>
2022-04-28 18:09:01 +0900

    게시글에 dno가 없는 현상 제거 toObject 메서드가 DiaryDto.kt를 생산하지만, dno가 ""인 채로 초기화 되므로 var로 다시 초기화해줄 수 있게 코드를 변경함.

--- app/src/main/java/com/goodee/cando_app/dto/DiaryDto.kt
+++ app/src/main/java/com/goodee/cando_app/dto/DiaryDto.kt
@@ -3,4 +3,4 @@ package com.goodee.cando_app.dto
 import java.io.Serializable
 import java.util.*
 
-data class DiaryDto(val dno: String? = "", val title: String = "", val content: String = "", val author: String = "", val date: Date = Date()) : Serializable
+data class DiaryDto(var dno: String? = "", val title: String = "", val content: String = "", val author: String = "", val date: Date = Date()) : Serializable
